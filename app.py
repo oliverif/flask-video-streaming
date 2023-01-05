b7a84f9b2348b4bf0979ef2762e8c28a31ed2666
oliveristadfunch <oliver.funch@cognite.com>
2023-01-05 20:50:55 +0100

    upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from importlib import import_module
 import os
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, url_for, redirect
 
 # import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+if os.environ.get("CAMERA"):
+    Camera = import_module("camera_" + os.environ["CAMERA"]).Camera
 else:
-    from camera import Camera
+    from camera_opencv import Camera
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
@@ -15,26 +15,34 @@ else:
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def index_post():
+    text = request.form["text"]
+    processed_text = text.upper()
+    return redirect(url_for("index"))
 
 
 def gen(camera):
     """Video streaming generator function."""
-    yield b'--frame\r\n'
+    yield b"--frame\r\n"
     while True:
         frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+        yield b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n--frame\r\n"
 
 
-@app.route('/video_feed')
+@app.route("/video_feed", defaults={"text": "default"})
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    print("video feed init")
+    cam = Camera()
+    return Response(gen(cam), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", threaded=True)
